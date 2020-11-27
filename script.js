84707357c51dcdaead0082d9a0e27669feb5268c
Unstrap Technologies <71831991+unstraptech@users.noreply.github.com>
2020-11-27 14:07:21 +0530

    Update 1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+     
+    sideNav.style.right = "-250px";
+    menuBtn.onclick = function(){
+    if(sideNav.style.right == "-250px")
+    {
+        sideNav.style.right == "0";
+    }
+    else{
+        sideNav.style.right == "-250px";
+    }
